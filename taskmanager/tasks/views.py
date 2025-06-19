69678843d4bcbff209e379c84ef96f54cdd6ca58
Divyank <dgharmalkar@enine.dev>
2025-06-19 18:09:09 +0530

    Refactor views to use class-based views

--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -1,19 +1,26 @@
-from django.shortcuts import render, redirect
+# views.py
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Task
-from .forms import TaskForm
+from django.urls import reverse_lazy
 
-def index(request):
-    tasks = Task.objects.all()
-    form = TaskForm()
+class TaskListView(ListView):
+    model = Task
+    template_name = 'task_list.html'
+    context_object_name = 'tasks'
 
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/')  # return after POST
+class TaskCreateView(CreateView):
+    model = Task
+    fields = ['title', 'description', 'completed']
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('task-list')
 
-    context = {
-        'tasks': tasks,
-        'form': form
-    }
-    return render(request, 'tasks/index.html', context)  # always return a response
+class TaskUpdateView(UpdateView):
+    model = Task
+    fields = ['title', 'description', 'completed']
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('task-list')
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'task_confirm_delete.html'
+    success_url = reverse_lazy('task-list')

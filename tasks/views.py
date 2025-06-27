74500e65f5312aa804af7588ea8a3598f2a0a919
Tanasvi Katolkar <tkatolkar@enine.dev>
2025-06-27 11:10:44 +0530

    feat(tasks): add view to render landing page

--- tasks/views.py
+++ tasks/views.py
@@ -1,107 +1,53 @@
 """Django views for managing tasks in a task management application."""
 
-from django.views.generic import ListView, CreateView, DetailView, TemplateView
-from django.core.paginator import Paginator
-from django.urls import reverse_lazy
-from django.shortcuts import render
-from .models import Task, List
-
-
+from django.views.generic import TemplateView
+from tasks.models import List, Task, SubTask
 
 
 class DashboardView(TemplateView):
-    template_name = 'tasks/task_list.html'
+    """
+    This view provides
+        1. lists
+        2. tasks in a list if selected else tasks in first available list
+        3. subtasks in a task if selected
+    """
+
+    template_name = "tasks/home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['lists'] = List.objects.order_by('-created_at')[:3]
+        lists = List.objects.all().order_by("-created_at")
+        selected_list, tasks = self.get_tasks(lists)
+        selected_task, subtasks = self.get_selected_task_details(tasks)
 
-        list_id = self.request.GET.get("list_id")
-        task_id = self.request.GET.get("task_id")
-        page = self.request.GET.get("page")
-
-        if task_id:
-            selected_task = Task.objects.get(id=task_id)
-            subtasks = selected_task.subtask_set.all().order_by("deadline")
-
-            paginator = Paginator(subtasks, 15)
-            page_obj = paginator.get_page(page)
-
-            context["selected_task"] = selected_task
-            context["subtasks"] = page_obj
-
-        elif list_id:
-            selected_list = List.objects.get(id=list_id)
-            
-
-            paginator = Paginator(selected_list, 7)
-            page_obj = paginator.get_page(page)
-
-            context["selected_list"] = selected_list
-            context["tasks"] = page_obj
+        context.update(
+            {
+                "lists": lists,
+                "selected_list": selected_list,
+                "tasks": tasks,
+                "selected_task": selected_task,
+                "subtasks": subtasks,
+            }
+        )
 
         return context
 
+    def get_tasks(self, lists):
+        if not lists:
+            return (List.objects.none(), Task.objects.none())
 
+        list_id = self.request.GET.get("list_id", lists.first().id)
+        return (
+            List.objects.get(id=list_id),
+            Task.objects.filter(list__id=list_id).order_by("-created_at"),
+        )
 
+    def get_selected_task_details(self, tasks):
+        if not tasks:
+            return (Task.objects.none(), SubTask.objects.none())
 
-
-class ListCreateView(CreateView):
-    model = List
-    fields = ["name"]
-    template_name = "tasks/list_form.html"
-    success_url = reverse_lazy("dashboard")
-
-
-class TaskListView(ListView):
-    model = Task
-    template_name = "tasks/task_list.html"
-    context_object_name = "tasks"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['lists'] = List.objects.order_by('-created_at')[:3]
-        return context
-
-# class ListDetailView(DetailView):
-#     model = List
-#     template_name = "tasks/list_detail.html"
-#     context_object_name = "list"
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         tasks = Task.objects.filter(list=self.object).order_by("deadline")
-#         print(tasks)
-#         paginator = Paginator(tasks, 7)  # Show 7 tasks per page
-#         page_number = self.request.GET.get('page')
-#         page_obj = paginator.get_page(page_number)
-#         context["tasks"] = page_obj
-#         return context
-
-    
-
-
-# class TaskCreateView(CreateView):
-#     """View for creating a new task."""
-
-#     model = Task
-#     fields = ["title", "description", "completed", "deadline"]
-#     template_name = "task_form.html"
-#     success_url = reverse_lazy("task_list")
-
-
-# class TaskUpdateView(UpdateView):
-#     """View for updating an existing task."""
-
-#     model = Task
-#     fields = ["title", "description", "completed", "deadline"]
-#     template_name = "task_form.html"
-#     success_url = reverse_lazy("task_list")
-
-
-# class TaskDeleteView(DeleteView):
-#     """View for deleting a task."""
-
-#     model = Task
-#     template_name = "task_confirm_delete.html"
-#     success_url = reverse_lazy("task_list")
+        task_id = self.request.GET.get("task_id", tasks.first().id)
+        return (
+            Task.objects.get(id=task_id),
+            SubTask.objects.filter(task__id=task_id).order_by("-created_at"),
+        )

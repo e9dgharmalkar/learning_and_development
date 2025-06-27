3bef07a2715ce4d95ea5e5c185b016ef5aee1717
Divyank <dgharmalkar@enine.dev>
2025-06-27 10:26:16 +0530

    Additions to Task Tracker, clickable lists.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,13 +1,16 @@
 """This module defines the URL patterns for the tasks app."""
 
 from django.urls import path
-from .views import TaskListView, dashboard, ListCreateView
+from .views import TaskListView, DashboardView, ListCreateView, DetailView
 
 
 urlpatterns = [
-    path("", dashboard, name="dashboard"),
+    path("", DashboardView.as_view(), name="dashboard"),
+    path('list/<int:pk>/', DashboardView.as_view(), name='list_detail'),
     path("add_list/", ListCreateView.as_view(), name="add_list"),
     path("tasks/", TaskListView.as_view(), name="task_list"),
+    path("dashboard/task/<int:pk>/", DetailView.as_view(), name="task_detail"),
+    path("task/<int:task_id>/", DashboardView.as_view(), name="list_detail"),
     # path('list/<int:list_id>/', views.list_detail, name='list_detail'),
     # path('add-list/', views.add_list, name='add_list'),
     # path("create/", TaskCreateView.as_view(), name="task_create"),
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 """Django views for managing tasks in a task management application."""
 
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, TemplateView
+from django.core.paginator import Paginator
 from django.urls import reverse_lazy
 from django.shortcuts import render
 from .models import Task, List
@@ -8,9 +9,41 @@ from .models import Task, List
 
 
 
-def dashboard(request):
-    lists = List.objects.order_by("-created_at")[:3]
-    return render(request, "tasks/task_list.html", {"lists": lists})
+class DashboardView(TemplateView):
+    template_name = 'tasks/task_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['lists'] = List.objects.order_by('-created_at')[:3]
+
+        list_id = self.request.GET.get("list_id")
+        task_id = self.request.GET.get("task_id")
+        page = self.request.GET.get("page")
+
+        if task_id:
+            selected_task = Task.objects.get(id=task_id)
+            subtasks = selected_task.subtask_set.all().order_by("deadline")
+
+            paginator = Paginator(subtasks, 15)
+            page_obj = paginator.get_page(page)
+
+            context["selected_task"] = selected_task
+            context["subtasks"] = page_obj
+
+        elif list_id:
+            selected_list = List.objects.get(id=list_id)
+            
+
+            paginator = Paginator(selected_list, 7)
+            page_obj = paginator.get_page(page)
+
+            context["selected_list"] = selected_list
+            context["tasks"] = page_obj
+
+        return context
+
+
+
 
 
 class ListCreateView(CreateView):
@@ -24,6 +57,28 @@ class TaskListView(ListView):
     model = Task
     template_name = "tasks/task_list.html"
     context_object_name = "tasks"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['lists'] = List.objects.order_by('-created_at')[:3]
+        return context
+
+# class ListDetailView(DetailView):
+#     model = List
+#     template_name = "tasks/list_detail.html"
+#     context_object_name = "list"
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         tasks = Task.objects.filter(list=self.object).order_by("deadline")
+#         print(tasks)
+#         paginator = Paginator(tasks, 7)  # Show 7 tasks per page
+#         page_number = self.request.GET.get('page')
+#         page_obj = paginator.get_page(page_number)
+#         context["tasks"] = page_obj
+#         return context
+
+    
 
 
 # class TaskCreateView(CreateView):

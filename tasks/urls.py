fbd87b72a5e7ac94fad6c206f80ee51457b5fb77
Tanasvi Katolkar <tkatolkar@enine.dev>
2025-07-01 10:54:05 +0530

    feat(tasks): add view to render landing page (#1)

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,19 +1,9 @@
 """This module defines the URL patterns for the tasks app."""
 
 from django.urls import path
-from .views import TaskListView, DashboardView, ListCreateView, DetailView
+from tasks.views import DashboardView
 
 
 urlpatterns = [
     path("", DashboardView.as_view(), name="dashboard"),
-    path('list/<int:pk>/', DashboardView.as_view(), name='list_detail'),
-    path("add_list/", ListCreateView.as_view(), name="add_list"),
-    path("tasks/", TaskListView.as_view(), name="task_list"),
-    path("dashboard/task/<int:pk>/", DetailView.as_view(), name="task_detail"),
-    path("task/<int:task_id>/", DashboardView.as_view(), name="list_detail"),
-    # path('list/<int:list_id>/', views.list_detail, name='list_detail'),
-    # path('add-list/', views.add_list, name='add_list'),
-    # path("create/", TaskCreateView.as_view(), name="task_create"),
-    # path("<int:pk>/update/", TaskUpdateView.as_view(), name="task_update"),
-    # path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task_delete"),
 ]
